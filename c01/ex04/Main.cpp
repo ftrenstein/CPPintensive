7c7997e3a03e55164055fd2f9d61855f53e1b795
ftrenstein <renstein@student.42.fr>
2023-08-07 11:38:32 +0300

    loser1

--- c01/ex04/Main.cpp
+++ c01/ex04/Main.cpp
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: renstein <renstein@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:01:57 by renstein          #+#    #+#             */
-/*   Updated: 2023/07/17 21:08:25 by renstein         ###   ########.fr       */
+/*   Updated: 2023/08/05 13:25:41 by renstein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Main.hpp"
+
 int main(int argc, char **argv)
 {
-    if (argc = 3)
+    if (argc == 4)
     {
-        
+        std::ofstream loser;
+        loser.open(argv[1]);
+        if (!loser.is_open())
+            std::cout << "error opening file" << std::endl;
+        else
+            loser << "copy";
+        loser.close();
     }
+    
     else
-        return (0);
+         std::cout << "wrong namber argv" << std::endl;;
     return (0);
 
 }
--- c01/ex04/Main.hpp
+++ c01/ex04/Main.hpp
@@ -1,12 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.hpp                                           :+:      :+:    :+:   */
+/*   Main.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: renstein <renstein@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:02:34 by renstein          #+#    #+#             */
-/*   Updated: 2023/07/17 21:02:35 by renstein         ###   ########.fr       */
+/*   Updated: 2023/08/05 13:20:26 by renstein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class X
+{
+private:
+    std::string _name;
+
+
+public:
+};
+
+#endif
